37e2869781d82c6b095d345f61e9e55dc3ce17ab
karayu80 <karayu80@gmail.com>
2022-02-28 16:09:01 +0900

    [add] docker hub test

--- hwd.py
+++ hwd.py
@@ -2,8 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return 'Web App with Python Flask!'
 
-app.run(host='0.0.0.0', port=81)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=81)
